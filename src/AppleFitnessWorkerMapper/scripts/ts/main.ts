003114f53923038cbb7f2dd162bdcbb92ded1c4f
martincostello <martin@martincostello.com>
2021-05-06 18:12:23 +0100

    Remove inline JavaScript
    Remove the inline JavaScript and instead add it to the main TypeScript file.

--- src/AppleFitnessWorkerMapper/scripts/ts/main.ts
+++ src/AppleFitnessWorkerMapper/scripts/ts/main.ts
@@ -1,5 +1,13 @@
-const foo = () => {
-
+const initializer = () => {
+    const london = { lat: 51.50809, lng: -0.1285907 };
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: london,
+    });
+    new google.maps.Marker({
+        position: london,
+        map: map,
+    });
 };
 
-foo();
+google.maps.event.addDomListener(window, 'load', initializer);

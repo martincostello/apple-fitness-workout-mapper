6d2117addf0d2799af65bfe111332a2017a8e519
martincostello <martin@martincostello.com>
2021-05-09 19:15:21 +0100

    Use options
    Use options instead of IConfiguration.

--- src/AppleFitnessWorkoutMapper/Pages/Index.cshtml.cs
+++ src/AppleFitnessWorkoutMapper/Pages/Index.cshtml.cs
@@ -2,15 +2,15 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace MartinCostello.AppleFitnessWorkoutMapper.Pages
 {
     public class IndexModel : PageModel
     {
-        public IndexModel(IConfiguration configuration)
+        public IndexModel(IOptions<ApplicationOptions> options)
         {
-            GoogleMapsApiKey = configuration["GoogleMapsApiKey"];
+            GoogleMapsApiKey = options.Value.GoogleMapsApiKey;
         }
 
         public string GoogleMapsApiKey { get; }

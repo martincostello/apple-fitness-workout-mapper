3f9462cb959df8dcc34bcfa7d94359dc6962a959
martincostello <martin@martincostello.com>
2021-05-28 12:16:24 +0100

    Wait after filtering
    Add temporary delay to see if that fixes the macOS tests.

--- tests/AppleFitnessWorkoutMapper.Tests/Pages/ApplicationPage.cs
+++ tests/AppleFitnessWorkoutMapper.Tests/Pages/ApplicationPage.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Martin Costello, 2021. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,6 +26,9 @@ namespace MartinCostello.AppleFitnessWorkoutMapper.Pages
             await _page.ClickAsync(FilterSelector);
             await _page.WaitUntilEnabledAsync(FilterSelector);
             await _page.WaitUntilHiddenAsync(LoaderSelector);
+
+            // HACK Temporary delay to see if that fixes the macOS tests
+            await Task.Delay(TimeSpan.FromSeconds(3));
         }
 
         public async Task ImportDataAsync()

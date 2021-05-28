0dd871ad3ef6ba6104fdfe05811fb6dcfa3a8407
martincostello <martin@martincostello.com>
2021-05-28 08:36:41 +0100

    Restore fluent API
    Add some extension methods to allow (easier) chaining of methods in a fluent way with Playwright.

---
+++ tests/AppleFitnessWorkoutMapper.Tests/TaskExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Martin Costello, 2021. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading.Tasks;
+
+namespace MartinCostello.AppleFitnessWorkoutMapper
+{
+    internal static class TaskExtensions
+    {
+        public static async Task<T> ThenAsync<T>(this Task<T> value, Func<T, Task<T>> continuation)
+        {
+            return await continuation(await value);
+        }
+
+        public static async Task ThenAsync<T>(this Task<T> value, Func<T, Task> continuation)
+        {
+            await continuation(await value);
+        }
+    }
+}

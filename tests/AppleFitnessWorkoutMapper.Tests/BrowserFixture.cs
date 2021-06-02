44f54c23db48ed7afeeb6905dc504eaa027b813f
martincostello <martin@martincostello.com>
2021-06-02 15:47:05 +0100

    Fix file names
    If a channel is used, replace any ':' so that the screenshot/video file names are valid.

--- tests/AppleFitnessWorkoutMapper.Tests/BrowserFixture.cs
+++ tests/AppleFitnessWorkoutMapper.Tests/BrowserFixture.cs
@@ -102,6 +102,8 @@ namespace MartinCostello.AppleFitnessWorkoutMapper
                 OperatingSystem.IsWindows() ? "windows" :
                 "other";
 
+            browserType = browserType.Replace(";", string.Empty, StringComparison.Ordinal);
+
             string utcNow = DateTimeOffset.UtcNow.ToString("yyyy-MM-dd-HH-mm-ss", CultureInfo.InvariantCulture);
             return $"{testName}_{browserType}_{os}_{utcNow}{extension}";
         }

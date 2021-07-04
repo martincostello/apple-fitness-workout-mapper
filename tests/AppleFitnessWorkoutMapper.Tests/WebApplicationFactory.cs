e450197fa49b28cd59c632b3f524591c565481af
martincostello <martin@martincostello.com>
2021-07-04 15:14:21 +0100

    Simplify tests
    Simplify the tests by using UseSolutionRelativeContentRoot().

--- tests/AppleFitnessWorkoutMapper.Tests/WebApplicationFactory.cs
+++ tests/AppleFitnessWorkoutMapper.Tests/WebApplicationFactory.cs
@@ -4,11 +4,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using MartinCostello.Logging.XUnit;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -44,7 +43,7 @@ namespace MartinCostello.AppleFitnessWorkoutMapper
             builder.ConfigureAppConfiguration((p) => p.AddInMemoryCollection(config))
                    .ConfigureLogging((p) => p.AddXUnit(this))
                    .ConfigureServices((p) => p.AddSingleton<IClock>((_) => new NodaTime.Testing.FakeClock(utcNow)))
-                   .UseContentRoot(GetApplicationContentRootPath());
+                   .UseSolutionRelativeContentRoot(Path.Combine("src", "AppleFitnessWorkoutMapper"));
         }
 
         protected override void Dispose(bool disposing)
@@ -59,17 +58,6 @@ namespace MartinCostello.AppleFitnessWorkoutMapper
             {
                 // Ignore
             }
-        }
-
-        private string GetApplicationContentRootPath()
-        {
-            var attribute = GetTestAssemblies()
-                .SelectMany((p) => p.GetCustomAttributes<WebApplicationFactoryContentRootAttribute>())
-                .Where((p) => string.Equals(p.Key, "AppleFitnessWorkoutMapper", StringComparison.OrdinalIgnoreCase))
-                .OrderBy((p) => p.Priority)
-                .First();
-
-            return attribute.ContentRootPath;
         }
     }
 }

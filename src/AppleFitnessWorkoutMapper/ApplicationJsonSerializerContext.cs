b9bf9def89b9175444e1d70f0d18b4b62ff29ad9
martincostello <martin@martincostello.com>
2021-11-27 14:31:26 +0000

    Use JSON source generator with Minimal APIs
    Add extension methods to plug the JSON source generator into Minimal APIs.

---
+++ src/AppleFitnessWorkoutMapper/ApplicationJsonSerializerContext.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Martin Costello, 2021. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+using MartinCostello.AppleFitnessWorkoutMapper.Models;
+
+namespace MartinCostello.AppleFitnessWorkoutMapper;
+
+[ExcludeFromCodeCoverage]
+[JsonSerializable(typeof(IList<Track>))]
+[JsonSerializable(typeof(TrackCount))]
+internal sealed partial class ApplicationJsonSerializerContext : JsonSerializerContext
+{
+}
---
+++ src/AppleFitnessWorkoutMapper/Models/TrackCount.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Martin Costello, 2021. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Text.Json.Serialization;
+
+namespace MartinCostello.AppleFitnessWorkoutMapper.Models;
+
+public sealed class TrackCount
+{
+    [JsonPropertyName("count")]
+    public int Count { get; set; }
+}

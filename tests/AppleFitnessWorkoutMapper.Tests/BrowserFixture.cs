da4ce31f56bbf5752fcdbda2e83957d9ff205658
martincostello <martin@martincostello.com>
2023-04-20 10:02:58 +0100

    Set UseArtifactsOutput=true
    Use the new `UseArtifactsOutput` property for the output paths and adjust the build scripts accordingly.

--- tests/AppleFitnessWorkoutMapper.Tests/BrowserFixture.cs
+++ tests/AppleFitnessWorkoutMapper.Tests/BrowserFixture.cs
@@ -9,7 +9,7 @@ namespace MartinCostello.AppleFitnessWorkoutMapper;
 public class BrowserFixture
 {
     private const string VideosDirectory = "videos";
-    private const string AssetsDirectory = ".";
+    private static readonly string AssetsDirectory = Path.Combine("..", "..", "..");
 
     public BrowserFixture(
         BrowserFixtureOptions options,

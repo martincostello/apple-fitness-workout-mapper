98b3a65f71b676a44c10491d1d3b31a81433e7fd
martincostello <martin@martincostello.com>
2024-12-21 11:06:09 +0000

    Migrate to xunit v3
    Migrate tests to use xunit v3.

--- tests/AppleFitnessWorkoutMapper.Tests/AppTests.cs
+++ tests/AppleFitnessWorkoutMapper.Tests/AppTests.cs
@@ -9,17 +9,15 @@ namespace MartinCostello.AppleFitnessWorkoutMapper;
 
 public class AppTests(ITestOutputHelper outputHelper)
 {
-    public ITestOutputHelper OutputHelper { get; set; } = outputHelper;
-
     [Fact]
     public async Task Can_Load_Homepage()
     {
         // Arrange
-        using var fixture = new WebApplicationFactory(OutputHelper);
+        using var fixture = new WebApplicationFactory(outputHelper);
         using var client = fixture.CreateClient();
 
         // Act
-        string actual = await client.GetStringAsync("/");
+        string actual = await client.GetStringAsync("/", TestContext.Current.CancellationToken);
 
         // Assert
         actual.ShouldNotBeNull();
@@ -31,30 +29,31 @@ public class AppTests(ITestOutputHelper outputHelper)
     public async Task Can_Import_Tracks_And_Query()
     {
         // Arrange
-        using var fixture = new WebApplicationFactory(OutputHelper);
+        var cancellationToken = TestContext.Current.CancellationToken;
+        using var fixture = new WebApplicationFactory(outputHelper);
 
         using var client = fixture.CreateClient();
 
         // Act
-        var result = await client.GetFromJsonAsync<CountResponse>("/api/tracks/count");
+        var result = await client.GetFromJsonAsync<CountResponse>("/api/tracks/count", cancellationToken);
 
         // Assert
         result.ShouldNotBeNull();
         result.Count.ShouldBe(0);
 
         // Act
-        using var response = await client.PostAsJsonAsync("/api/tracks/import", new { });
+        using var response = await client.PostAsJsonAsync("/api/tracks/import", new { }, cancellationToken);
 
         // Assert
         response.StatusCode.ShouldBe(HttpStatusCode.Created);
 
-        result = await response.Content.ReadFromJsonAsync<CountResponse>();
+        result = await response.Content.ReadFromJsonAsync<CountResponse>(cancellationToken);
 
         result.ShouldNotBeNull();
         result.Count.ShouldBe(2);
 
         // Act
-        var actual = await client.GetFromJsonAsync<IList<Track>>("api/tracks");
+        var actual = await client.GetFromJsonAsync<IList<Track>>("api/tracks", cancellationToken);
 
         // Assert
         actual.ShouldNotBeNull();
@@ -65,7 +64,7 @@ public class AppTests(ITestOutputHelper outputHelper)
         item.Timestamp.ShouldBe(new DateTimeOffset(2021, 05, 04, 11, 25, 35, TimeSpan.Zero));
 
         // Act
-        actual = await client.GetFromJsonAsync<IList<Track>>("api/tracks?notBefore=2021-05-05T00:00:00Z");
+        actual = await client.GetFromJsonAsync<IList<Track>>("api/tracks?notBefore=2021-05-05T00:00:00Z", cancellationToken);
 
         // Assert
         actual.ShouldNotBeNull();
